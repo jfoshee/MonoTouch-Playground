c7a1017b99692e35ee7b1686ac7478f830a762e5
Jacob Foshee <jacobf@gmail.com>
2012-08-02 15:28:03 -0500

    Making this example as simple as possible

--- NoXib/SimpleController.cs
+++ NoXib/SimpleController.cs
@@ -1,23 +1,12 @@
-using System;
-using System.Drawing;
-using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 
 namespace NoXib
 {
     public class SimpleController : UIViewController
     {		
-        public override void ViewDidLoad()
+        public SimpleController()
         {
-            base.ViewDidLoad();
-            View.BackgroundColor = UIColor.White;
-            View.AddSubview(new UILabel(View.Bounds) { Text = "Hello" });
-        }
-
-        public override bool ShouldAutorotateToInterfaceOrientation(UIInterfaceOrientation toInterfaceOrientation)
-        {
-            return true;
+            View = new SimpleView();
         }
     }
 }
-
---
+++ NoXib/SimpleView.cs
@@ -0,0 +1,12 @@
+using MonoTouch.UIKit;
+
+namespace NoXib
+{
+    public class SimpleView : UIView
+    {
+        public SimpleView()
+        {
+            BackgroundColor = UIColor.Green;
+        }
+    }
+}
