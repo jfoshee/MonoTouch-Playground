9663bf50248afefd4d01f3375201579a1218cfc8
Jacob Foshee <jacobf@gmail.com>
2012-12-05 16:36:07 -0600

    Playing with storyboards

---
+++ DetailStoryboardSample/DetailViewController.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Drawing;
+using System.Collections.Generic;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace DetailStoryboardSample
+{
+    public partial class DetailViewController : UIViewController
+    {
+        string detailItem;
+		
+        [Export("detailItem")]
+        public string DetailItem
+        {
+            get
+            {
+                return detailItem;
+            }
+            set
+            {
+                detailItem = value;
+                ConfigureView();
+            }
+        }
+		
+        public DetailViewController(IntPtr handle) : base (handle)
+        {
+        }
+		
+        void ConfigureView()
+        {
+            // Update the user interface for the detail item
+            if (DetailItem != null)
+                this.detailDescriptionLabel.Text = DetailItem.ToString();
+        }
+		
+        public override void DidReceiveMemoryWarning()
+        {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning();
+			
+            // Release any cached data, images, etc that aren't in use.
+        }
+		
+		#region View lifecycle
+		
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+			
+            // Perform any additional setup after loading the view, typically from a nib.
+            ConfigureView();
+        }
+		
+        public override void ViewDidUnload()
+        {
+            base.ViewDidUnload();
+			
+            // Clear any references to subviews of the main view in order to
+            // allow the Garbage Collector to collect them sooner.
+            //
+            // e.g. myOutlet.Dispose (); myOutlet = null;
+			
+            ReleaseDesignerOutlets();
+        }
+		
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+        }
+		
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+        }
+		
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+        }
+		
+        public override void ViewDidDisappear(bool animated)
+        {
+            base.ViewDidDisappear(animated);
+        }
+		
+		#endregion
+		
+        public override bool ShouldAutorotateToInterfaceOrientation(UIInterfaceOrientation toInterfaceOrientation)
+        {
+            // Return true for supported orientations
+            return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
+        }
+    }
+}
+
